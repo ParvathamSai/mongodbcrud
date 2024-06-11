7dde868d6494838c1ba90fc99605a90b0a4d3f47
ParvathamSai <parvathamsai1@gmail.com>
2024-06-11 14:06:49 +0530

    initial Commit

---
+++ url.js
@@ -0,0 +1,2 @@
+// module.exports = `mongodb://localhost:27017`
+module.exports = 'mongodb+srv://admin:admin@cluster0.2mqgwnt.mongodb.net/'//we have to use this cloud db if we want to deploy our api
