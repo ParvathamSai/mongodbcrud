b8214b57eb126488566f6252f95b9cfc05b8ba4b
ParvathamSai <parvathamsai1@gmail.com>
2024-06-13 16:54:27 +0530

    initial Commit

--- delete/delete.js
+++ delete/delete.js
@@ -3,39 +3,95 @@ const express = require('express')
 let mongodb = require('mongodb')
 //import url
 const url = require('../url')
-//create mongo client
+//create mongoclient
 let mcl = mongodb.MongoClient
 //create router instance
 let router = express.Router()
-//create rest api
-router.delete("/", (req, res) => {
-    let obj = {
-        "p_id": req.body.p_id
-    }
-    //connect to mongodb
-    mcl.connect(url, (err, conn) => {
-        if (err)
+//database name
+let dbName = 'miniprj'
+//create restapi
+router.post("/", (req, res) => {
+	let obj = {
+    	"p_id": req.body.p_id
+	}
+	//connect to mongodb
+	mcl.connect(url, (err, conn) => {
+    	if (err)
             console.log('Error in connection:- ', err)
-        else {
-            let db = conn.db("nodedb")
-            db.collection('products').deleteOne(obj, (err, result) => {
+    	else {
+        	let db = conn.db(dbName)
+            db.collection('products').deleteOne({"p_id":req.body.p_id}, (err, result) => {
                 if (err)
-                    res.json({ 'delete': 'Error ' + err })
+ 	               res.json({ 'delete': 'Error ' + err })
                 else {
                     if (result.deletedCount != 0) {
-                        console.log("Data deleted ")
-                        res.json({ 'delete': 'success' })
+                    	console.log("Data deleted ")
+                    	res.json({ 'delete': 'success' })
                     } else {
-                        console.log("Data Not deleted ")
-                        res.json({ 'delete': 'Record Not found' })
+                    	console.log("Data Not deleted ")
+                    	res.json({ 'delete': 'Record Not found' })
+                    }
+                    conn.close()
+                }
+        	})
+    	}
+	})
+})
+//Delete product from cart
+router.post("/deleteCart", (req, res) => {
+	let obj = {
+    	"p_id": req.body.p_id,
+    	"u_name": req.body.u_name
+	}
+	//connect to mongodb
+    mcl.connect(url, (err, conn) => {
+    	if (err)
+            console.log('Error in connection:- ', err)
+    	else {
+        	let db = conn.db(dbName)
+            db.collection('cart').deleteOne(obj, (err, result) => {
+                if (err)
+                    res.json({ 'cartDelete': 'Error ' + err })
+                else {
+                    if (result.deletedCount != 0) {
+                    	console.log(`Cart data from ${obj.u_name} deleted`)
+                    	res.json({ 'cartDelete': 'success' })
+                    }
+                    else {
+                    	console.log('Cart Data Not deleted')
+                    	res.json({ 'cartDelete': 'Record Not found' })
                     }
                     conn.close()
                 }
             })
-        }
-    })
+    	}
+	})
 })
 
-
+//Delete user
+router.post("/deleteUser", (req, res) => {
+	//connect to mongodb
+	mcl.connect(url, (err, conn) => {
+    	if (err)
+            console.log('Error in connection:- ', err)
+    	else {
+        	let db = conn.db(dbName)
+            db.collection('users').deleteOne({"u_name":req.body.u_name,"upwd":req.body.upwd}, (err, result) => {
+                if (err)
+ 	               res.json({ 'delete': 'Error ' + err })
+                else {
+                    if (result.deletedCount != 0) {
+                    	console.log("Data deleted ")
+                    	res.json({ 'delete': 'success' })
+                    } else {
+                    	console.log("Data Not deleted ")
+                    	res.json({ 'delete': 'Record Not found' })
+                    }
+                    conn.close()
+                }
+        	})
+    	}
+	})
+})
 //export router
 module.exports = router
--- update/update.js
+++ update/update.js
@@ -3,42 +3,114 @@ const express = require('express')
 let mongodb = require('mongodb')
 //import url
 const url = require('../url')
-//create mongo client
+//create mongoclient
 let mcl = mongodb.MongoClient
 //create router instance
 let router = express.Router()
-//create rest api
-router.put("/", (req, res) => {
-    let p_id = req.body.p_id
-    let obj = {
-        p_name: req.body.p_name,
-        p_cost: req.body.p_cost
-    }
-    //connect to mongodb
+//database name
+let dbName = 'miniprj'
+//create restapi
+router.post("/", (req, res) => {
+	let p_id = req.body.p_id
+	let obj = {
+        "p_id" : req.body.p_id,
+    	"p_name": req.body.p_name,
+    	"p_cost": req.body.p_cost,
+        "p_cat": req.body.p_cat,
+        "p_img": req.body.p_img,
+        "p_desc": req.body.p_desc
+	}
+	//connect to mongodb
     mcl.connect(url, (err, conn) => {
-        if (err)
-            console.log('Error in connection :- ', err)
-        else {
-            let db = conn.db('nodedb')
+    	if (err)
+            console.log('Error in connection:- ', err)
+    	else {
+        	let db = conn.db(dbName)
             db.collection('products').updateOne({ p_id }, { $set: obj }, (err, result) => {
                 if (err)
                     res.json({ 'update': 'Error ' + err })
-                else{
+                else {
                     if (result.matchedCount != 0) {
-                        console.log("Data updated ")
-                        res.json({ 'update': 'success' })
+                    	console.log("Data updated ")
+                    	res.json({ 'update': 'success' })
                     } else {
-                        console.log("Data Not updated ")
-                        res.json({ 'update': 'Record Not found' })
+                    	console.log("Data Not updated ")
+                    	res.json({ 'update': 'Record Not found' })
                     }
                     conn.close()
                 }
-            })
-        }
-    })
+        	})
+    	}
+	})
 })
-
+//Update product in cart
+router.post("/updateCart", (req, res) => {
+	let p_id = req.body.p_id
+	let u_name = req.body.u_name
+	let obj = { "qty": req.body.qty }
+	//connect to mongodb
+    mcl.connect(url, (err, conn) => {
+    	if (err)
+            console.log('Error in connection:- ', err)
+    	else {
+        	let db = conn.db(dbName)
+            db.collection('cart').updateOne({ p_id, u_name }, { $set: obj },
+                (err, result) => {
+                    if (err)
+                    	res.json({ 'cartUpdate': 'Error ' + err })
+                    else {
+                    	if (result.matchedCount != 0) {
+                        	console.log(`Cart data for ${u_name} updated`)
+                        	res.json({ 'cartUpdate': 'success' })
+                    	}
+                    	else {
+                            console.log(`Record not updated`)
+                        	res.json({ 'cartUpdate': 'Record Not found' })
+                    	}
+                    	conn.close()
+                    }
+                })
+    	}
+	})
+})
+ 
+//Update user
+router.post("/updateUser", (req, res) => {
+	let u_name = req.body.u_name
+    let upwd=req.body.upwd
+	let obj = {
+        "userid" : req.body.userid,
+    	"u_name": req.body.u_name,
+    	"upwd": req.body.upwd,
+        "u_email": req.body.u_email,
+        "u_addr": req.body.u_addr,
+        "u_contact": req.body.u_contact
+	}
+	//connect to mongodb
+    mcl.connect(url, (err, conn) => {
+    	if (err)
+            console.log('Error in connection:- ', err)
+    	else {
+        	let db = conn.db(dbName)
+            db.collection('users').updateOne({ u_name, upwd }, { $set: obj },
+                (err, result) => {
+                    if (err)
+                    	res.json({ 'userUpdate': 'Error ' + err })
+                    else {
+                    	if (result.matchedCount != 0) {
+                        	console.log(`user data for ${u_name} updated`)
+                        	res.json({ 'userUpdate': 'success' })
+                    	}
+                    	else {
+                            console.log(`Record not updated`)
+                        	res.json({ 'userUpdate': 'Record Not found' })
+                    	}
+                    	conn.close()
+                    }
+                })
+    	}
+	})
+})
 
 //export router
 module.exports = router
-
